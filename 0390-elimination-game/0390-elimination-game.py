2022dc3c451d509d71d48908096760e2e33983d1
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-09 13:06:27 +0530

    Time: 0 ms (100%), Space: 12.2 MB (92.02%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lastRemaining(self, n):
+        head = 1
+        step = 1
+        left_to_right = True
+
+        while n > 1:
+            if left_to_right or n % 2 == 1:
+                head += step
+
+            n //= 2
+            step *= 2
+            left_to_right = not left_to_right
+
+        return head
