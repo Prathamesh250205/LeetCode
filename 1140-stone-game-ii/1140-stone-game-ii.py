be7b8cc94943d126d49b4e093c93bc1570b0c56c
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-09 12:56:40 +0530

    Time: 139 ms (93.75%), Space: 13.6 MB (63.54%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def stoneGameII(self, piles):
+        n = len(piles)
+        suffix = [0] * (n + 1)
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
+        memo = {}
+
+        def dp(i, M):
+            if i >= n:
+                return 0
+            if i + 2 * M >= n:
+                return suffix[i]
+            key = (i, M)
+            if key in memo:
+                return memo[key]
+            best = 0
+            for X in range(1, 2 * M + 1):
+                best = max(best, suffix[i] - dp(i + X, max(M, X)))
+            memo[key] = best
+            return best
+
+        return dp(0, 1)
