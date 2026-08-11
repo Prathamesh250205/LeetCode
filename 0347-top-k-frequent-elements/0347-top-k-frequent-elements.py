f017aa338baba77ee9556a8af90ca923bb52b286
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-11 23:39:30 +0530

    Time: 11 ms (56.14%), Space: 14.1 MB (87.76%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+import heapq
+from collections import Counter
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        return heapq.nlargest(k, count.keys(), key=count.get)
