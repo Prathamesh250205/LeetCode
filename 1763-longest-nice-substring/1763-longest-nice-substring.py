84171246360dadf4442e30493e9f04f5ec0e0c13
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-15 19:27:32 +0530

    Time: 1 ms (87.78%), Space: 12.6 MB (20.56%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestNiceSubstring(self, s):
+        if len(s) < 2:
+            return ""
+
+        char_set = set(s)
+        for i, c in enumerate(s):
+            if c.swapcase() not in char_set:
+                left = self.longestNiceSubstring(s[:i])
+                right = self.longestNiceSubstring(s[i + 1:])
+                return left if len(left) >= len(right) else right
+
+        return s
