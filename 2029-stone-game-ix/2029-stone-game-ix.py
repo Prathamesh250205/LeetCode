cd69510dd664b9ea610d872b6cca46fdb058873c
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-16 21:18:28 +0530

    Time: 60 ms (42.86%), Space: 20.1 MB (85.71%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        cnt = [0, 0, 0]
+        for s in stones:
+            cnt[s % 3] += 1
+        if cnt[0] % 2 == 0:
+            return cnt[1] > 0 and cnt[2] > 0
+        return cnt[1] > cnt[2] + 2 or cnt[2] > cnt[1] + 2
