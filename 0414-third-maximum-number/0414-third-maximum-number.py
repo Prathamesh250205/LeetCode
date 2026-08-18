5c7887b6669f0e5dca67eeb0531ad1106ee3a288
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-18 18:42:42 +0530

    Time: 0 ms (100%), Space: 13 MB (44.11%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        distinct = list(set(nums))
+        if len(distinct) < 3:
+            return max(distinct)
+        distinct.sort(reverse=True)
+        return distinct[2]
