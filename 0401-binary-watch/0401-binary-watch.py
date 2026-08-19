dcebc1289c96c7143fb008d732a8866657f0dac3
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-19 20:31:30 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.95%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        result = []
+        for h in range(12):
+            for m in range(60):
+                if bin(h).count('1') + bin(m).count('1') == turnedOn:
+                    result.append('%d:%02d' % (h, m))
+        return result
