66eaa65eed4919ddff8f2b82d9aa0bb9d12080a4
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-24 19:00:50 +0530

    Time: 10 ms (91.22%), Space: 16.1 MB (7.58%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findJudge(self, n, trust):
+        """
+        :type n: int
+        :type trust: List[List[int]]
+        :rtype: int
+        """
+        score = [0] * (n + 1)
+
+        for a, b in trust:
+            score[a] -= 1
+            score[b] += 1
+
+        for person in range(1, n + 1):
+            if score[person] == n - 1:
+                return person
+
+        return -1
