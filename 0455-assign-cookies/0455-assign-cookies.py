5e512d747b6c1df9668b40af721e02cc189f482c
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-29 23:19:52 +0530

    Time: 36 ms (29.62%), Space: 14 MB (87.96%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s.sort()
+
+        child = 0
+        cookie = 0
+        content = 0
+
+        while child < len(g) and cookie < len(s):
+            if s[cookie] >= g[child]:
+                content += 1
+                child += 1
+            cookie += 1
+
+        return content
