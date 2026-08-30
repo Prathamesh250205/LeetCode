9a518051bd605c62ccfbc20b281fcb86bb78361a
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-08-30 23:26:18 +0530

    Time: 10 ms (98.17%), Space: 13.9 MB (19.14%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        def serialize(node):
+            if not node:
+                return "#"
+            return "^" + str(node.val) + " " + serialize(node.left) + " " + serialize(node.right)
+
+
+        root_str = serialize(root)
+        sub_str = serialize(subRoot)
+
+        return sub_str in root_str
