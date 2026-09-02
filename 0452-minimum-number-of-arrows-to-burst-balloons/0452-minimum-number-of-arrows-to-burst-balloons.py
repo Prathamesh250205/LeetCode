3dce3c00c86ec233904029651712deb056bd1d86
Prathamesh250205 <gulavaniprathamesh@gmail.com>
2026-09-02 17:38:57 +0530

    Time: 128 ms (76.61%), Space: 44 MB (35.57%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findMinArrowShots(self, points):
+        if not points:
+            return 0
+        
+        points.sort(key=lambda x: x[1])
+        arrows = 1
+        prev_end = points[0][1]
+        
+        for start, end in points[1:]:
+            if start > prev_end:
+                arrows += 1
+                prev_end = end
+                
+        return arrows
